625bb3973842530aa53c132e7a610f8e7ecc3edf
Amelia Duclos <ameliaduclos@MacBook-Pro-de-Felipe.local>
2026-06-16 16:45:33 -0500

    add hero section

--- macbook-website/src/components/NavBar.jsx
+++ macbook-website/src/components/NavBar.jsx
@@ -4,28 +4,28 @@ import { navLinks } from '../constants'
 const NavBar = () => {
   return (
     <header>
-        <nav>
-            <img src="/logo.svg" alt="Apple Logo" />
+      <nav>
+        <img src="/logo.svg" alt="Apple Logo" />
 
-            <ul>
-                {navLinks.map(( { label }) => (
-                    <li key={label}>
-                        <a href={label}>{label}</a>
-                    </li>
-                ))}
-            </ul>
+        <ul>
+          {navLinks.map(({ Label }) => (
+            <li key={Label}>
+              <a href={Label}>{Label}</a>
+            </li>
+          ))}
+        </ul>
 
-            <div classname="flex-center gap-3">
-                <button>
-                    <img src="/search.svg" alt="Search" />
-                </button>
-                <button>
-                    <img src="/cart.svg" alt="Cart" />
-                </button>
-            </div>
-        </nav>
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="Cart" />
+          </button>
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default NavBar
