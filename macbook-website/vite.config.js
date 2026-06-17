8bf542f4aabafeb22e37dbe098c4d50ca7edc92f
Amelia Duclos <ameliaduclos@MacBook-Pro-de-Felipe.local>
2026-06-17 09:45:27 -0500

    added render canvas for 3d

--- macbook-website/vite.config.js
+++ macbook-website/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      react: fileURLToPath(new URL('./node_modules/react', import.meta.url)),
+      'react-dom': fileURLToPath(new URL('./node_modules/react-dom', import.meta.url)),
+    },
+  },
 })
